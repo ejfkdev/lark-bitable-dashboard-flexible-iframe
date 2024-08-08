becac5ebdf03093b61af4f1ea395717568715659
ejfkdev <152914458+ejfkdev@users.noreply.github.com>
2024-08-08 19:13:43 +0800

    update: assetPrefix

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,9 @@
 import semi from '@douyinfe/semi-next'
 /** @type {import('next').NextConfig} */
-const nextConfig = { ...semi.default() };
+const nextConfig = {
+    ...semi.default(),
+    assetPrefix: './',
+    output: "export",
+};
 
 export default nextConfig;
